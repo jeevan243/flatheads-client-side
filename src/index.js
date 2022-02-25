8d75a53e77605e91a534117c0bed9c08f09dd94d
Duwesh Kumar <dkduwesh07771@gmail.com>
2022-02-26 01:10:11 +0530

    payment route changed for successfull and unsuccessful transaction

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const { register, signin } = require("./controllers/auth.controller");
 const passport = require("./config/google_oauth");
 const productsContrller = require("./controllers/product.controller");
 
+const https = require("https");
+const qs = require("querystring");
 const userController = require("./controllers/user.controller");
 
 const parseUrl = express.urlencoded({ extended: false });
@@ -160,6 +162,86 @@ app.post("/paynow", [parseUrl, parseJson], (req, res) => {
       }
     );
   }
+});
+
+//route for paytm received and verification
+app.post("/callback", (req, res) => {
+  // Route for verifiying payment
+
+  var body = "";
+
+  req.on("data", function (data) {
+    body += data;
+  });
+
+  req.on("end", function () {
+    var html = "";
+    var post_data = qs.parse(body);
+
+    // received params in callback
+    console.log("Callback Response: ", post_data, "\n");
+
+    // verify the checksum
+    var checksumhash = post_data.CHECKSUMHASH;
+    // delete post_data.CHECKSUMHASH;
+    var result = checksum_lib.verifychecksum(
+      post_data,
+      config.PaytmConfig.key,
+      checksumhash
+    );
+    console.log("Checksum Result => ", result, "\n");
+
+    // Send Server-to-Server request to verify Order Status
+    var params = { MID: config.PaytmConfig.mid, ORDERID: post_data.ORDERID };
+
+    checksum_lib.genchecksum(
+      params,
+      config.PaytmConfig.key,
+      function (err, checksum) {
+        params.CHECKSUMHASH = checksum;
+        post_data = "JsonData=" + JSON.stringify(params);
+
+        var options = {
+          hostname: "securegw-stage.paytm.in", // for staging
+          // hostname: 'securegw.paytm.in', // for production
+          port: 443,
+          path: "/merchant-status/getTxnStatus",
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Content-Length": post_data.length,
+          },
+        };
+
+        // Set up the request
+        var response = "";
+        var post_req = https.request(options, function (post_res) {
+          post_res.on("data", function (chunk) {
+            response += chunk;
+          });
+
+          post_res.on("end", function () {
+            console.log("S2S Response: ", response, "\n");
+
+            var _result = JSON.parse(response);
+            if (_result.STATUS == "TXN_SUCCESS") {
+              // res.send("payment sucess");
+              res.redirect("/thankyou");
+            } else {
+              // res.send("payment failed");
+              res.send(
+                '<!DOCTYPE html><html><head><title>Failed Transaction</title></head><body><h1>Transaction Failed, Please retry!!</h1><script>setTimeout(function () { window.location = "/checkout";}, 2000)</script></body></html>'
+              );
+            }
+          });
+        });
+
+        // post the data
+        post_req.write(post_data);
+        post_req.end();
+      }
+    );
+  });
 });
 
 //thankyou page
