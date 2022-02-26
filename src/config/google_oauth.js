6527ba3c32a93cf8057330a1f92bc49f3c96d156
Jeevan Prasad <95869206+jeevan243@users.noreply.github.com>
2022-02-26 21:59:02 +0530

    call back added in google Oauth

--- src/config/google_oauth.js
+++ src/config/google_oauth.js
@@ -4,12 +4,14 @@ const { v4: uuidv4 } = require("uuid");
 const User = require("../models/user.model");
 const GoogleStrategy = require("passport-google-oauth2").Strategy;
 
+const callback = "http://localhost:1234" || "https://flatheads-official.herokuapp.com/"
+
 passport.use(
   new GoogleStrategy(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:1234",
+      callbackURL: callback,
       passReqToCallback: true,
     },
 
